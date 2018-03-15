6b8c36377e9a2add7baaac682dd2c72782c7f609
Drew Russell <drew_russell@rubrik-rangers.local>
2018-03-15 14:15:09 -0500

    remvoe unused import from assign sla

--- library/rubrik_assign_sla.py
+++ library/rubrik_assign_sla.py
@@ -142,7 +142,7 @@ def main():
     module.exit_json(**results)
 
 
-from ansible.module_utils.basic import AnsibleModule, jsonify # isort:skip
+from ansible.module_utils.basic import AnsibleModule # isort:skip
 from ansible.module_utils.rubrik import load_provider_variables, rubrik_argument_spec, rubrik_get, rubrik_post, rubrik_patch  # isort:skip
 
 
