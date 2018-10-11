22e0d0e5da7ab7a37d80fdd23c7457521811f351
Drew Russell <Drew.Russell@rubrik.com>
2018-10-11 17:58:00 -0500

    add module template

---
+++ utils/rubrik_module_template.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# Copyright: Rubrik
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.rubrikcdm import sdk_validation, load_provider_variables, rubrik_argument_spec
+
+
+def main():
+    """ Main entry point for Ansible module execution.
+    """
+
+    argument_spec = rubrik_argument_spec
+
+    argument_spec.update(
+        dict(
+
+        )
+    )
+
+    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=False)
+
+    sdk_present, rubrik_cdm = sdk_validation()
+
+    if sdk_present is False:
+        module.fail_json(msg="The Rubrik Python SDK is required for this module (pip install rubrik_cdm).")
+
+    results = {}
+
+    load_provider_variables(module)
+    ansible = module.params
+
+    rubrik = rubrik_cdm.Connect(ansible['node'], ansible['username'], ansible['password'])
+
+    ##################################
+    ######### Code Block #############
+    ##################################
+    ##################################
+
+    module.exit_json(**results)
+
+
+if __name__ == '__main__':
+    main()
