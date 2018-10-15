16dbbabc0e94c555c7e68e6d5e9c6a3e767d388f
Drew Russell <Drew.Russell@rubrik.com>
2018-10-15 14:31:43 -0500

    add documentation to rubrik_assign_sla

--- library/rubrik_assign_sla.py
+++ library/rubrik_assign_sla.py
@@ -1,6 +1,52 @@
 #!/usr/bin/python
 # Copyright: Rubrik
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+ANSIBLE_METADATA = {
+    'metadata_version': '1.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+DOCUMENTATION = '''
+module: rubrik_assign_sla
+short_description: Assign a Rubrik object to an SLA Domain.
+description: Assign a Rubrik object to an SLA Domain.
+    - 
+version_added: '2.7'
+author: 'Rubrik Ranger Team'
+options:
+
+  object_name:
+    description:
+      - The name of the Rubrik object you wish to assign to an SLA Domain. 
+    required: true
+    type: str
+
+  sla_name:
+    description:
+      - The name of the SLA Domain you wish to assign an object to. To exclude the object from all SLA assignments use do not protect as the sla_name. To assign the selected object to the SLA of the next higher level object use clear as the sla_name
+    required: true
+    type: str
+
+  object_type:
+    description:
+      - The Rubrik object type you want to assign to the SLA Domain. 
+    required: false
+    default: vmware
+    choices: ['vmware]
+    type: str
+    
+ timeout:
+    description:
+      - The number of seconds to wait to establish a connection the Rubrik cluster before returning a timeout error.
+    required: False
+    default: 30
+    type: int
+
+extends_documentation_fragment:
+    - rubrik_cdm
+requirements: [rubrik_cdm]
+'''
 
 EXAMPLES = '''
 - rubrik_assign_sla:
