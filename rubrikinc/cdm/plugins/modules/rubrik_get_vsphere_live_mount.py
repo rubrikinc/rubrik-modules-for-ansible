32d5cc82d32ec1794e807a194b19fad85460a96d
Drew Russell <Drew.Russell@rubrik.com>
2020-01-19 22:40:34 +0000

    update import on vsphere_live_mount

--- rubrikinc/cdm/plugins/modules/rubrik_get_vsphere_live_mount.py
+++ rubrikinc/cdm/plugins/modules/rubrik_get_vsphere_live_mount.py
@@ -50,7 +50,7 @@ version:
     type: dict
 '''
 
-from ansible_collections.rubrikinc.cdm.plugins.module_utils.rubrik_cdm import credentials, load_provider_variables, rubrik_argument_spec
+from ansible.module_utils.rubrik_cdm import credentials, load_provider_variables, rubrik_argument_spec
 from ansible.module_utils.basic import AnsibleModule
 
 try:
